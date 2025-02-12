3e8554cfb041268064cbe4884068a5dc8e2673e6
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-02-12 20:05:31 +0545

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -3,12 +3,12 @@ import "dotenv/config";
 import cors from "cors";
 
 import router from "./routes/index.mjs";
-import connectDB from "./connections/index.mjs";
+import { connectMongoDB } from "./connections/index.mjs";
 
 const PORT = process.env.PORT || 8080 // Use hard coded PORT 8080 if intended PORT is unavailable
 const MONGODB_URL = process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/derasewa"
 
-connectDB(MONGODB_URL)
+connectMongoDB(MONGODB_URL)
 
 const app = express();
 app.use(express.json());
